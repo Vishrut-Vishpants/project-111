c342ed05bcee3f088b9a710734b17c22c0b1ba2c
Vishrut-Vishpants <95662816+Vishrut-Vishpants@users.noreply.github.com>
2023-07-06 16:53:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function takesnapshot(){
 }
 
 console.log("ml5 version",ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MWFV9cLYE/model.json",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/i_nt2E8UN/model.json",modelloaded);
 
 function modelloaded(){
     console.log("modeloaded");
